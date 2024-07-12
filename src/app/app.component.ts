c9e4f3eca7fd092aaef5dccaea115db72dc73a88
triad-pedro-bastos <pedro.bastos@triadpesquisa.com.br>
2024-07-12 17:59:27 -0300

    Adicionando formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+})
+export class FormComponent implements OnInit {
+  results = [];
+  formGroup!: FormGroup;
+
+  constructor() {}
+
+  onSubmitForm() {
+    console.log(this.formGroup);
+  }
+
+  onCleanForm() {}
+
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      hour: new FormControl(''),
+      showCLosedUnits: new FormControl(false),
+    });
+  }
+}
